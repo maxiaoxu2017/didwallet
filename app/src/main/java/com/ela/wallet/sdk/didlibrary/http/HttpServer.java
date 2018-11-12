13f1b3be353eb1496103d263e7d4e36de2238252
maxiaoxu <maxiaoxu2017@163.com>
2018-11-12 10:40:43 +0800

    fix bug:dex merge failed

--- app/src/main/java/com/ela/wallet/sdk/didlibrary/http/HttpServer.java
+++ app/src/main/java/com/ela/wallet/sdk/didlibrary/http/HttpServer.java
@@ -42,27 +42,20 @@ public class HttpServer extends NanoHTTPD {
     @Override
     public Response serve(IHTTPSession session) {
         LogUtil.w("httpd serve");
-        /*我在这里做了一个限制，只接受POST请求。这个是项目需求。*/
         if (Method.POST.equals(session.getMethod())) {
             Map<String, String> files = new HashMap<String, String>();
-            /*获取header信息，NanoHttp的header不仅仅是HTTP的header，还包括其他信息。*/
             Map<String, String> header = session.getHeaders();
-
             try {
-                /*这句尤为重要就是将将body的数据写入files中，大家可以看看parseBody具体实现，倒现在我也不明白为啥这样写。*/
                 session.parseBody(files);
-                /*看就是这里，POST请教的body数据可以完整读出*/
                 String body = session.getQueryParameterString();
                 LogUtil.d("header : " + header);
                 LogUtil.d("body : " + body);
-                /*这里是从header里面获取客户端的IP地址。NanoHttpd的header包含的东西不止是HTTP heaer的内容*/
                 header.get("http-client-ip");
             } catch (IOException e) {
                 e.printStackTrace();
             } catch (ResponseException e) {
                 e.printStackTrace();
             }
-            /*这里就是为客户端返回的信息了。我这里返回了一个200和一个HelloWorld*/
             return newFixedLengthResponse(NanoHTTPD.Response.Status.OK, "text/html", "HelloWorld");
         }else {
             String uri = session.getUri();
@@ -70,8 +63,8 @@ public class HttpServer extends NanoHTTPD {
             String params = session.getQueryParameterString();
             if (uri.startsWith("/did")) {
                 String address = Utilty.getPreference(null, Constants.SP_KEY_DID_ADDRESS, "");
-                return newFixedLengthResponse(Response.Status.OK, "application/json", String.format("{\"status\":\"200\",\"did\":\"%s\"}", address));
-            } else if (uri.startsWith("/banlence")) {
+                return newFixedLengthResponse(Response.Status.OK, "application/json", String.format("{\"status\":\"200\",\"result\":\"%s\"}", address));
+            } else if (uri.startsWith("/balance")) {
                 return newFixedLengthResponse(Response.Status.OK, "application/json", String.format("{\"status\":\"200\",\"result\":\"%s\"}", 0));
             }
 
